2e2dd3bbb229db2b93ba651a4331c1dc3c0acf64
Riemenschneider, Tobias Dr <Tobias.Riemenschneider@softwareag.com>
2021-04-16 14:25:31 +0200

    Organized imports.

--- src/test/java/MariaDBConnectionPool.java
+++ src/test/java/MariaDBConnectionPool.java
@@ -1,6 +1,4 @@
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
